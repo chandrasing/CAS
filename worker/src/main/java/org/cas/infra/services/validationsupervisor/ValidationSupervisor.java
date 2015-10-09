3e05be6edab150c792408b69ea839402f1b2f1bc
Chandrasing Patil <chandrasing.patil@gmail.com>
2015-10-09 12:44:56 +0530

    - GPL Licence header added to most of the source files. - Added licence plugin to automate licence management

--- worker/src/main/java/org/cas/infra/services/validationsupervisor/ValidationSupervisor.java
+++ worker/src/main/java/org/cas/infra/services/validationsupervisor/ValidationSupervisor.java
@@ -1,3 +1,22 @@
+/**
+ * CAS - https://github.com/chandrasing/CAS
+ *
+ * Copyright (C) 2015 Chandrasing
+ * Copyright (C) 2015 contributors
+ *
+ * CAS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * CAS is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CAS; if not, see <http://www.gnu.org/licenses/>.
+ */
 package org.cas.infra.services.validationsupervisor;
 
 import org.cas.infra.BaseActor;
